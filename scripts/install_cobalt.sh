898303bfa193ec2859aac8a46d4aaacf8d277341
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-09 13:50:01 +0100

    Install COBALT

---
+++ scripts/install_cobalt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Installs COBALT using the R code in the 'cobaltr' package
+#
+# Because the R code in the package uses 'cobaltr::' in calling
+# its functions, this script removes these prefixes.
+#
+# Usage:
+#
+#  ./scripts/install_cobalt.sh
+#
+#
+cat $(find | egrep "/R/.*\\.R") > tempscript.R
+
+echo "install_cobalt()" >> tempscript.R
+sed -i'.orginal' "s/cobaltr:://g" tempscript.R
+cat tempscript.R
+Rscript tempscript.R
+rm tempscript.R
+rm tempscript.R.orginal
