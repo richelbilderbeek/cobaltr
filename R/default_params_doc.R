6e94089b93552b4b9ee14c7bb0e3d480dbbd5717
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-10 07:58:44 +0100

    Fix doc

--- R/default_params_doc.R
+++ R/default_params_doc.R
@@ -4,6 +4,8 @@
 #' @param cobalt_folder the folder where COBALT is installed in.
 #' Use \link{get_default_cobalt_folder} to get the default COBALT folder
 #' @param fasta_filename name of a FASTA file
+#' @param matrix_name name of the COBALT matrix.
+#'   Use \link{get_matrix_names} to get a list of all possible matrix names
 #' @param verbose if TRUE, additional information is displayed, that
 #'   is potentially useful in debugging
 #' @author Richèl J.C. Bilderbeek
@@ -13,6 +15,7 @@
 default_params_doc <- function(
   cobalt_folder,
   fasta_filename,
+  matrix_name,
   verbose
 ) {
   # Nothing
