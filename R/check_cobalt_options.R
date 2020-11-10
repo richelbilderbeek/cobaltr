b2b1a39bed56aceed6085f2c8e60cc11e71a13a6
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-10 11:01:10 +0100

    Check data types of cobalt_options

---
+++ R/check_cobalt_options.R
@@ -0,0 +1,141 @@
+#' Check if the argument is a valid \code{cobalt_options}
+#'
+#' Will \link{stop} with a helpful error message if not
+check_cobalt_options <- function(cobalt_options) {
+  if (!is.character(cobalt_options$search_pattern_filename)) {
+    stop(
+      "'search_pattern_filename' must be a filename. \n",
+      "Actual class: ", class(cobalt_options$search_pattern_filename)
+    )
+  }
+  if (!is.character(cobalt_options$domain_database_filename)) {
+    stop(
+      "'domain_database_filename' must be a filename. \n",
+      "Actual class: ", class(cobalt_options$domain_database_filename)
+    )
+  }
+  if (!is.character(cobalt_options$conserved_block_filename)) {
+    stop(
+      "'conserved_block_filename' must be a filename. \n",
+      "Actual class: ", class(cobalt_options$conserved_block_filename)
+    )
+  }
+  if (!is.character(cobalt_options$residue_frequencies_filename)) {
+    stop(
+      "'residue_frequencies_filename' must be a filename. \n",
+      "Actual class: ", class(cobalt_options$residue_frequencies_filename)
+    )
+  }
+  if (!is.character(cobalt_options$destination_filename)) {
+    stop(
+      "'destination_filename' must be a filename. \n",
+      "Actual class: ", class(cobalt_options$destination_filename)
+    )
+  }
+  if (!is.character(cobalt_options$pairwise_alignment_constraints_filename)) {
+    stop(
+      "'pairwise_alignment_constraints_filename' must be a filename. \n",
+      "Actual class: ", class(cobalt_options$pairwise_alignment_constraints_filename)
+    )
+  }
+  if (!is.numeric(cobalt_options$conserved_domain_threshold)) {
+    stop(
+      "'conserved_domain_threshold' must be a number. \n",
+      "Actual class: ", class(cobalt_options$conserved_domain_threshold)
+    )
+  }
+  if (!is.numeric(cobalt_options$filler_threshold)) {
+    stop(
+      "'filler_threshold' must be a number. \n",
+      "Actual class: ", class(cobalt_options$filler_threshold)
+    )
+  }
+  if (!is.numeric(cobalt_options$terminal_open_gap_penalty)) {
+    stop(
+      "'terminal_open_gap_penalty' must be a number. \n",
+      "Actual class: ", class(cobalt_options$terminal_open_gap_penalty)
+    )
+  }
+  if (!is.numeric(cobalt_options$terminal_extend_gap_penalty)) {
+    stop(
+      "'terminal_extend_gap_penalty' must be a number. \n",
+      "Actual class: ", class(cobalt_options$terminal_extend_gap_penalty)
+    )
+  }
+  if (!is.numeric(cobalt_options$open_gap_in_middle_penalty)) {
+    stop(
+      "'open_gap_in_middle_penalty' must be a number. \n",
+      "Actual class: ", class(cobalt_options$open_gap_in_middle_penalty)
+    )
+  }
+  if (!is.numeric(cobalt_options$middle_extend_gap_penalty)) {
+    stop(
+      "'middle_extend_gap_penalty' must be a number. \n",
+      "Actual class: ", class(cobalt_options$middle_extend_gap_penalty)
+    )
+  }
+  if (!is.numeric(cobalt_options$conservation_score_threshold)) {
+    stop(
+      "'conservation_score_threshold' must be a number. \n",
+      "Actual class: ", class(cobalt_options$conservation_score_threshold)
+    )
+  }
+  if (!is.numeric(cobalt_options$dbf_added_weight)) {
+    stop(
+      "'dbf_added_weight' must be a number. \n",
+      "Actual class: ", class(cobalt_options$dbf_added_weight)
+    )
+  }
+  if (!is.numeric(cobalt_options$ffb_added_weight)) {
+    stop(
+      "'ffb_added_weight' must be a number. \n",
+      "Actual class: ", class(cobalt_options$ffb_added_weight)
+    )
+  }
+  if (!is.logical(cobalt_options$no_rps)) {
+    stop(
+      "'no_rps' must be TRUE or FALSE. \n",
+      "Actual class: ", class(cobalt_options$no_rps)
+    )
+  }
+  if (!is.logical(cobalt_options$verbose)) {
+    stop(
+      "'verbose' must be TRUE or FALSE. \n",
+      "Actual class: ", class(cobalt_options$verbose)
+    )
+  }
+  if (!is.logical(cobalt_options$search_around_conserved_columns)) {
+    stop(
+      "'search_around_conserved_columns' must be TRUE or FALSE. \n",
+      "Actual class: ", class(cobalt_options$search_around_conserved_columns)
+    )
+  }
+  if (!is.character(cobalt_options$score_matrix_name)) {
+    stop(
+      "'score_matrix_name' must be a string. \n",
+      "Actual class: ", class(cobalt_options$score_matrix_name)
+    )
+  }
+  if (!is.numeric(cobalt_options$pseudocount_constant)) {
+    stop(
+      "'pseudocount_constant' must be a number \n",
+      "Actual class: ", class(cobalt_options$pseudocount_constant)
+    )
+  }
+  if (!is.logical(cobalt_options$use_fastme)) {
+    stop(
+      "'use_fastme' must be TRUE or FALSE. \n",
+      "Actual class: ", class(cobalt_options$use_fastme)
+    )
+  }
+  if (!is.logical(cobalt_options$do_dry_run)) {
+    stop(
+      "'do_dry_run' must be TRUE or FALSE. \n",
+      "Actual class: ", class(cobalt_options$do_dry_run)
+    )
+  }
+
+
+
+
+}
---
+++ tests/testthat/test-check_cobalt_options.R
@@ -0,0 +1,201 @@
+test_that("use", {
+  expect_silent(check_cobalt_options(create_cobalt_options()))
+})
+
+test_that("search_pattern_filename", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(search_pattern_filename = 42)
+    ),
+    "'search_pattern_filename' must be a filename"
+  )
+})
+
+test_that("domain_database_filename", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(domain_database_filename = 42)
+    ),
+    "'domain_database_filename' must be a filename"
+  )
+})
+
+test_that("conserved_block_filename", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(conserved_block_filename = 42)
+    ),
+    "'conserved_block_filename' must be a filename"
+  )
+})
+
+test_that("residue_frequencies_filename", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(residue_frequencies_filename = 42)
+    ),
+    "'residue_frequencies_filename' must be a filename"
+  )
+})
+
+test_that("destination_filename", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(destination_filename = 42)
+    ),
+    "'destination_filename' must be a filename"
+  )
+})
+
+test_that("pairwise_alignment_constraints_filename", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(pairwise_alignment_constraints_filename = 42)
+    ),
+    "'pairwise_alignment_constraints_filename' must be a filename"
+  )
+})
+
+test_that("conserved_domain_threshold", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(conserved_domain_threshold = "nonsense")
+    ),
+    "'conserved_domain_threshold' must be a number"
+  )
+})
+
+test_that("filler_threshold", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(filler_threshold = "nonsense")
+    ),
+    "'filler_threshold' must be a number"
+  )
+})
+
+test_that("terminal_open_gap_penalty", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(terminal_open_gap_penalty = "nonsense")
+    ),
+    "'terminal_open_gap_penalty' must be a number"
+  )
+})
+
+test_that("terminal_extend_gap_penalty", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(terminal_extend_gap_penalty = "nonsense")
+    ),
+    "'terminal_extend_gap_penalty' must be a number"
+  )
+})
+
+test_that("open_gap_in_middle_penalty", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(open_gap_in_middle_penalty = "nonsense")
+    ),
+    "'open_gap_in_middle_penalty' must be a number"
+  )
+})
+
+test_that("middle_extend_gap_penalty", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(middle_extend_gap_penalty = "nonsense")
+    ),
+    "'middle_extend_gap_penalty' must be a number"
+  )
+})
+
+test_that("conservation_score_threshold", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(conservation_score_threshold = "nonsense")
+    ),
+    "'conservation_score_threshold' must be a number"
+  )
+})
+
+test_that("use", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(dbf_added_weight = "nonsense")
+    ),
+    "'dbf_added_weight' must be a number"
+  )
+})
+
+test_that("ffb_added_weight", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(ffb_added_weight = "nonsense")
+    ),
+    "'ffb_added_weight' must be a number"
+  )
+})
+
+test_that("no_rps", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(no_rps = "nonsense")
+    ),
+    "'no_rps' must be TRUE or FALSE"
+  )
+})
+
+test_that("use", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(verbose = "nonsense")
+    ),
+    "'verbose' must be TRUE or FALSE"
+  )
+})
+
+test_that("use", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(search_around_conserved_columns = "nonsense")
+    ),
+    "'search_around_conserved_columns' must be TRUE or FALSE"
+  )
+})
+
+test_that("score_matrix_name", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(score_matrix_name = 42)
+    ),
+    "'score_matrix_name' must be a string"
+  )
+})
+
+test_that("use", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(pseudocount_constant = "nonsense")
+    ),
+    "'pseudocount_constant' must be a number"
+  )
+})
+
+test_that("use", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(use_fastme = "nonsense")
+    ),
+    "'use_fastme' must be TRUE or FALSE"
+  )
+})
+
+test_that("use", {
+  expect_error(
+    check_cobalt_options(
+      create_cobalt_options(do_dry_run = "nonsense")
+    ),
+    "'do_dry_run' must be TRUE or FALSE"
+  )
+})
