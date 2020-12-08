1000a3a1026f795adea22157ebd968786b933b0f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-08 13:18:59 +0100

    Add main help page

---
+++ R/cobaltr.R
@@ -0,0 +1,10 @@
+#' \code{cobaltr}: Interface to COBALT.
+#'
+#' \code{cobaltr} allows to call COBALT, a popular
+#' multiple alignment tool, using
+#' an R interface. 'cobaltr' closely follows the interface
+#' of COBALT, including its default settings.
+#'
+#' @docType package
+#' @name cobaltr
+NULL
